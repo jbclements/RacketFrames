c46efaa843fa483d5f36d809946897066bded182
Shubham Kahal <shubhamkahal@Shubhams-iMac.local>
2020-12-10 00:21:50 -0800

    add dataframe/ back into omit paths

--- info.rkt
+++ info.rkt
@@ -1,15 +1,17 @@
 #lang info
+
 (define collection 'RacketFrames 'multi)
 (define version "1.0")
+
 (define deps '("typed-racket-lib"               
                "plot-lib"))
 (define build-deps '("racket-doc"
                      "scribble-lib"))
 
-(define compile-omit-paths '("tests/"                            
-                             "benchmark/"                         
-                             "validation/"
-                             "jupyter-notebooks/"
-                             "sample-csv/"                            
-                             "util/future-work/"
-                             "stats/future-work/"))
+(define compile-omit-paths '("dataframe/tests/"                            
+                             "dataframe/benchmark/"                         
+                             "dataframe/validation/"
+                             "dataframe/jupyter-notebooks/"
+                             "dataframe/sample-csv/"                            
+                             "dataframe/util/future-work/"
+                             "dataframe/stats/future-work/"))

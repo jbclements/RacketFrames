24265e2d625109aede5e5653be1994f221304bcd
John Clements <clements@racket-lang.org>
2020-12-18 15:46:27 -0800

    WIP dataframes reorg

---
+++ info.rkt
@@ -0,0 +1,22 @@
+#lang setup/infotab
+
+(define collection 'multi)
+
+#;(define deps (list (list "portaudio" "0.1") 
+                   "base"
+                   "data-lib"
+                   "gui-lib"
+                   "htdp-lib"
+                   "math-lib"
+                   "plot-lib"
+                   "plot-gui-lib"
+                   "rackunit-lib"
+                   "typed-racket-lib"
+                   "drracket-plugin-lib"
+                   "memoize"
+                   "pict-lib"
+                   "wxme-lib"
+                   "snip-lib"))
+#;(define build-deps (list "scribble-lib"
+                         "racket-doc"
+                         "wxme-lib"))
